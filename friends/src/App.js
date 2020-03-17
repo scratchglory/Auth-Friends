f4edc87a4f23fd8fa5ade632be65d8ebdbbfe8e0
adella <scratchglory@gmail.com>
2020-03-17 12:40:43 -0400

    created add friend form

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Login from "./components/Login";
 import FriendsList from "./components/FriendsList";
 import PrivateRoute from "./components/PrivateRoute";
+import Post from "./components/Post";
 
 function App() {
   return (
@@ -17,13 +18,18 @@ function App() {
           <li>
             <Link to="/protected">PROTECTED</Link>
           </li>
+          <li>
+            <Link to="/add-friend">ADD FRIEND</Link>
+          </li>
         </ul>
         <Switch>
           {/* Protected Route */}
           <PrivateRoute exact path="/protected" component={FriendsList} />
           {/* must have both login routes in order to work */}
           <Route path="/login" component={Login} />
+          <Route path="/add-friend" component={Post} />
           <Route component={Login} />
+          <Route component={Post} />
         </Switch>
       </div>
     </Router>
--- friends/src/components/Post.js
+++ friends/src/components/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import axios from "axios";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 class Post extends React.Component {
   state = {
@@ -18,6 +19,35 @@ class Post extends React.Component {
       age: this.state.age,
       email: this.state.email
     };
+    // axios.post method takes two arguments
+    // 1. url
+    // 2. 'data' we need to post to the BE server, then chaned with 'then' and 'catch' method
+    axios
+      .post("http://localhost:5000/add-friend", data)
+      .then(res => console.log(res))
+      .catch(err => console.log("POST-ERROR: ", err));
+  };
+
+  idChange = e => {
+    this.setState({
+      id: e.target.value
+    });
+  };
+  nameChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  ageChange = e => {
+    this.setState({
+      age: e.target.value
+    });
+  };
+  emailChange = e => {
+    this.setState({
+      email: e.target.value
+    });
   };
 
   // create form
@@ -25,12 +55,35 @@ class Post extends React.Component {
     return (
       <div className="post">
         <form onSubmit={this.handleSubmit}>
-          <input name="id" value={this.state.id} />
-          <input name="name" value={this.state.name} />
-          <input name="age" value={this.state.age} />
-          <input name="email" value={this.state.email} />
+          <input
+            name="id"
+            value={this.state.id}
+            onChange={this.idChange}
+            placeholder="id"
+          />
+          <input
+            name="name"
+            value={this.state.name}
+            onChange={this.nameChange}
+            placeholder="name"
+          />
+          <input
+            name="age"
+            value={this.state.age}
+            onChange={this.ageChange}
+            placeholder="age"
+          />
+          <input
+            name="email"
+            value={this.state.email}
+            onChange={this.emailChange}
+            placeholder="email"
+          />
+          <button type="submit">HELLO NEW FRIEND!</button>
         </form>
       </div>
     );
   }
 }
+
+export default Post;
